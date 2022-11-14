1992d01c55d7a4f338575ce261060bab5e9c6262
JangKyy <114386052+JangKyy@users.noreply.github.com>
2022-11-14 10:39:17 +0700

    Add files via upload

---
+++ application/views/data_kelas.php
@@ -0,0 +1,37 @@
+<!-- Content Header (Page header) -->
+<div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Data Kelas</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Data kelas</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-12">
+          <div class="card">
+              <div class="card-header border-0">
+                <div class="d-flex justify-content-between">
+                  <h3 class="card-title">Data Kelas</h3>
+                </div>
+              </div>
+              <div class="card-body">
+                  <table class="table table-striped">
+
+                  </table>
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+      </div>
+    </div>
